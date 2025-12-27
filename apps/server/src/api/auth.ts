7434a746ea3f88bec7f2e97ba1497b2eda650247
Ratwin <mikkelljungberg@gmail.com>
2025-12-27 23:19:27 +0100

    fix openapi and add better auth

--- apps/server/src/api/auth.ts
+++ apps/server/src/api/auth.ts
@@ -1,7 +1,7 @@
 import { createHonoApp } from '../app/hono/create-hono-app.ts';
 import { auth } from '../lib/auth.ts';
 
-const authentication = createHonoApp().on(['POST', 'GET'], '/auth/*', (c) =>
+const authentication = createHonoApp().on(['POST', 'GET'], '/api/auth/*', (c) =>
   auth.handler(c.req.raw)
 );
 
--- apps/server/src/app/app.ts
+++ apps/server/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Scalar } from '@scalar/hono-api-reference';
-import { authentication } from '../resources/auth.ts';
+import { authentication } from '../api/auth.ts';
 import { createHonoApp } from './hono/create-hono-app.ts';
 
 const app = createHonoApp().route('/', authentication);
 // .route('/dropzone', dropzones);
 
 // OpenAPI Documentation Setup
-app.doc('/openapi', {
+app.doc('/api/open-api', {
   openapi: '3.1.2',
   info: {
     title: 'Blomsterkanten API',
@@ -24,11 +24,11 @@ app.get(
     theme: 'deepSpace',
     sources: [
       {
-        url: '/openapi',
+        url: '/api/open-api',
         title: 'Blomsterkanten API',
       },
       {
-        url: '/auth/open-api/generate-schema',
+        url: '/api/auth/open-api/generate-schema',
         title: 'Blomsterkanten Authentication API',
       },
     ],
